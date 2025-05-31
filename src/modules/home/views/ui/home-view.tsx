0c1f95ae4dc3c38990f7076f2f9b39c217329afa
MrSachin7 <nepalghi@gmail.com>
2025-05-31 21:17:02 +0200

    Add sidebar

--- src/modules/home/views/ui/home-view.tsx
+++ src/modules/home/views/ui/home-view.tsx
@@ -5,12 +5,10 @@ import { useRouter } from "next/navigation";
 import React from "react";
 
 const HomeView = () => {
-  const { data: session } = authClient.useSession();
   const router = useRouter();
 
   return (
     <div className="flex flex-col p-4 gap-y-4">
-      <p>Logged in as {session?.user.name}</p>
       <Button
         onClick={() =>
           authClient.signOut({
