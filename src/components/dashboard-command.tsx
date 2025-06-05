c57132db339656c23ac2d9a04159f440eda6e45b
MrSachin7 <nepalghi@gmail.com>
2025-06-05 18:06:31 +0200

    Responsive dialog

--- src/components/dashboard-command.tsx
+++ src/components/dashboard-command.tsx
@@ -1,6 +1,6 @@
 import React, { Dispatch } from "react";
 import {
-  CommandDialog,
+  CommandResponsiveDialog,
   CommandInput,
   CommandItem,
   CommandList,
@@ -13,12 +13,12 @@ interface DashboardCommandProps {
 
 const DashboardCommand = ({ open, setOpen }: DashboardCommandProps) => {
   return (
-    <CommandDialog open={open} onOpenChange={setOpen}>
+    <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
       <CommandInput placeholder="Find a meeting or agent" />
       <CommandList>
-        <CommandItem>Test</CommandItem>
+        <CommandItem></CommandItem>
       </CommandList>
-    </CommandDialog>
+    </CommandResponsiveDialog>
   );
 };
 
--- src/modules/home/views/ui/home-view.tsx
+++ src/modules/home/views/ui/home-view.tsx
@@ -1,7 +1,33 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
+import { authClient } from "@/lib/auth-client";
 import React from "react";
 
 const HomeView = () => {
-  return <div>Home view</div>;
+  return (
+    <div>
+      Home view
+      <Button
+        variant={"link"}
+        onClick={() => {
+          authClient.signOut({
+            fetchOptions: {
+              onSuccess: () => {
+                // Redirect to sign-in page after logout
+                window.location.href = "/sign-in";
+              },
+              onError: (error) => {
+                console.error("Logout failed:", error);
+              },
+            },
+          });
+        }}
+      >
+        Logout
+      </Button>
+    </div>
+  );
 };
 
 export default HomeView;
